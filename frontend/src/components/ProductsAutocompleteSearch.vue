11734b311f673f8737d6b3c3a6a1dd557917ef7e
Kiril <kiril2315@gmail.com>
2024-05-03 18:10:12 +0300

    products autocomplete some error and loading handling

--- frontend/src/components/ProductsAutocompleteSearch.vue
+++ frontend/src/components/ProductsAutocompleteSearch.vue
@@ -7,17 +7,25 @@ const props = defineProps({
 });
 
 const products = ref<ProductItem[]>([]);
+const loading = ref(true);
+const errorFetching = ref(false);
 
 const fetchProducts = async () => {
+  loading.value = true;
   if (!props.searchQ) {
     products.value = [];
     return;
   }
   try {
+    // TODO better api search get handling
     const resp = await axios.get(`api/products/search/byname/${props.searchQ}`);
+    errorFetching.value = false;
     products.value = resp.data;
   } catch (error) {
+    errorFetching.value = true;
     console.error("Failed to fetch products:", error);
+  } finally {
+    loading.value = false;
   }
 };
 
@@ -31,14 +39,15 @@ watch(
 </script>
 
 <template>
-  <div class="m-2">
-    <div v-for="product in products">
-      <div class="">
+  <div class="m-2" :class="{ 'animate-pulse': loading }">
+    <div v-for="product in products" v-if="!errorFetching">
+      <div>
         <h1 class="text-slate-700">{{ product.product_name }}</h1>
         <img :src="product.product_image_url" alt="" />
         <p>Price: ${{ product.product_price }}</p>
       </div>
     </div>
+    <div v-else class="flex justify-center">Error fetching</div>
   </div>
 </template>
 
