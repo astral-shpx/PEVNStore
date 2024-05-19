537d3c2a37134cb0459eb4e7fa4109c070f56e6b
Kiril <kiril2315@gmail.com>
2024-05-19 13:17:32 +0300

    rm unused

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,22 +1,11 @@
-import { createApp, markRaw } from "vue";
+import { createApp } from "vue";
 import "./style.css";
 import router from "./router";
 import App from "./App.vue";
 import { createPinia } from "pinia";
-import type { Router } from "vue-router";
-
-declare module "pinia" {
-  export interface PiniaCustomProperties {
-    router: Router;
-  }
-}
 
 const pinia = createPinia();
 const app = createApp(App);
-
-pinia.use(({ store }) => {
-  store.router = markRaw(router);
-});
 
 app.use(router);
 app.use(pinia);
