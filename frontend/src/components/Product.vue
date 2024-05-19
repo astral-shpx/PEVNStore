477658350f06be73e43d4bf9a26ca538e26e7de2
Kiril <kiril2315@gmail.com>
2024-05-19 11:42:42 +0300

    fix product styles text overflow

--- frontend/src/components/Product.vue
+++ frontend/src/components/Product.vue
@@ -21,8 +21,8 @@ const navigateToProduct = async () => {
     class="flex flex-col items-center justify-center mb-4 cursor-pointer"
     @click="navigateToProduct"
   >
-    <div>
-      <h1>{{ product.product_name }}</h1>
+    <div class="w-full object-scale-down">
+      <h1 class="text-ellipsis overflow-hidden">{{ product.product_name }}</h1>
       <img :src="product.product_image_url" alt="" />
       <p>Price: ${{ product.product_price }}</p>
     </div>
