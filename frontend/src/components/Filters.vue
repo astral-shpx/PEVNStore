7805f39f133e49abb7e360341e9b9c0eda465b31
Kiril <kiril2315@gmail.com>
2024-05-21 22:28:04 +0300

    filters html scaffold

--- frontend/src/components/Filters.vue
+++ frontend/src/components/Filters.vue
@@ -1,10 +1,24 @@
 <script setup lang="ts">
-import { watch } from "vue";
+import { watch, ref } from "vue";
 import useProductStore from "../piniaStores/useProductsStore";
 
 const productsStore = useProductStore();
 
 const { filters, fetchProducts } = productsStore;
+const priceRanges = ref([
+  { label: "$0 - $50", min: 0, max: 50 },
+  { label: "$51 - $100", min: 51, max: 100 },
+  { label: "$101 - $200", min: 101, max: 200 },
+  { label: "$201 - $500", min: 201, max: 500 },
+  { label: "$501 - $1000", min: 501, max: 1000 },
+]);
+const selectedPriceRanges = ref([]);
+const rating = ref(3);
+const ratingMin = ref(0);
+const ratingMax = ref(5);
+const sortBy = ref("reviews_asc");
+const loadAmount = ref(10);
+const loadAmounts = ref([12, 24, 30]);
 
 const updateFromDate = (event: Event) => {
   filters.fromDate = (event.target as HTMLInputElement).value;
@@ -44,25 +58,68 @@ watch(filters, async () => {
         @input="updateToDate"
       />
     </div>
-    <div class="flex flex-col">
+
+    <div class="flex flex-col mb-4">
       <h3 class="mb-2">Price</h3>
-      <!-- price checkbox -->
+      <div class="flex flex-col">
+        <label v-for="priceRange in priceRanges" :key="priceRange.label">
+          <input
+            type="checkbox"
+            v-model="selectedPriceRanges"
+            :value="priceRange"
+          />
+          {{ priceRange.label }}
+        </label>
+      </div>
     </div>
-    <div class="flex flex-col">
-      <h3 class="mb-2">Rating</h3>
-      <!-- product rating slider -->
+
+    <div class="flex flex-col mb-4">
+      <h3 class="mb-2">Rating above</h3>
+      <input
+        type="range"
+        v-model="rating"
+        :min="ratingMin"
+        :max="ratingMax"
+        step="0.1"
+      />
+      <p>{{ rating }}</p>
     </div>
+
     <div class="flex flex-col">
       <h3 class="mb-2">Category</h3>
       <!-- category dropdown -->
     </div>
-    <div class="flex flex-col">
+
+    <!-- sort by reviews[dropdown] asc / desc -->
+    <div class="flex flex-col mb-4">
       <h3 class="mb-2">Sort by</h3>
-      <!-- sort by reviews[dropdown] asc / desc -->
+      <label>
+        <input type="radio" />
+        Ascending
+      </label>
+      <label>
+        <input type="radio" />
+        Descending
+      </label>
+      <select class="dark:text-slate-900" v-model="sortBy">
+        <option class="mx-2" value="reviews_asc">Reviews (Asc)</option>
+        <option class="mx-2" value="reviews_desc">Reviews (Desc)</option>
+      </select>
     </div>
-    <div class="flex flex-col">
+
+    <div class="flex flex-col mb-4">
       <h3 class="mb-2">Load amount</h3>
-      <!-- load amount radio -->
+      <div>
+        <label v-for="amount in loadAmounts" :key="amount">
+          <input
+            class="mx-1"
+            type="radio"
+            v-model="loadAmount"
+            :value="amount"
+          />
+          {{ amount }}
+        </label>
+      </div>
     </div>
   </div>
 </template>
