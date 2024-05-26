92f78a23a15566e38109853ca60e16a60354702c
Kiril <kiril2315@gmail.com>
2024-05-26 18:02:41 +0300

    better styles for login, sign up forms

--- frontend/src/pages/AccountPage.vue
+++ frontend/src/pages/AccountPage.vue
@@ -16,87 +16,101 @@ const logIn = async () => {
 </script>
 
 <template>
-  <h1>Account</h1>
+  <div class="flex justify-center">
+    <div class="flex flex-col px-2 md:w-1/2 justify-center items-center">
+      <h1>Account</h1>
 
-  <div>Sign up</div>
-  <form @submit.prevent="signUp">
-    <div class="mb-2">
-      <label for="username">Username:</label>
-      <input
-        class="text-black"
-        type="text"
-        id="username"
-        v-model="userStore.username"
-        required
-      />
+      <!-- forms -->
+      <!-- sign up form -->
+      <h3>Sign up</h3>
+      <form @submit.prevent="signUp" class="mb-4">
+        <div class="m-2 flex justify-between">
+          <label class="mx-2" for="username">Username</label>
+          <input
+            class="text-black rounded-md border-2 border-slate-500"
+            type="text"
+            id="username"
+            v-model="userStore.username"
+            required
+          />
+        </div>
+        <div class="m-2 flex justify-between">
+          <label class="mx-2" for="email">Email</label>
+          <input
+            class="text-black rounded-md border-2 border-slate-500"
+            type="email"
+            id="email"
+            v-model="userStore.email"
+            required
+          />
+        </div>
+        <div class="m-2 flex justify-between">
+          <label class="mx-2" for="password">Password</label>
+          <input
+            class="text-black rounded-md border-2 border-slate-500"
+            type="password"
+            id="password"
+            v-model="userStore.password"
+            required
+          />
+        </div>
+        <div class="mt-4 flex justify-center w-full">
+          <button
+            class="outline-dashed rounded-sm dark:hover:bg-slate-700 hover:bg-slate-400 w-2/3"
+            type="submit"
+          >
+            Sign Up
+          </button>
+        </div>
+      </form>
+
+      <!-- user info -->
+      <p v-if="userStore.message">{{ userStore.message }}</p>
+
+      <!-- log out button -->
+      <div v-if="userStore.user">
+        <p>Logged in as {{ userStore.user }}</p>
+
+        <button
+          @click="userStore.logOut"
+          class="outline-dashed rounded-sm dark:hover:bg-slate-700 hover:bg-slate-400"
+        >
+          Log out
+        </button>
+      </div>
+
+      <!-- log in form -->
+      <h3>Log in</h3>
+      <form @submit.prevent="logIn">
+        <div class="m-2 flex justify-between">
+          <label class="mx-2" for="username">Username</label>
+          <input
+            class="text-black rounded-md border-2 border-slate-500"
+            type="text"
+            id="username"
+            v-model="login_credentials.username"
+            required
+          />
+        </div>
+        <div class="m-2 flex justify-between">
+          <label class="mx-2" for="password">Password</label>
+          <input
+            class="text-black rounded-md border-2 border-slate-500"
+            type="password"
+            id="password"
+            v-model="login_credentials.password"
+            required
+          />
+        </div>
+        <div class="mt-4 flex justify-center w-full">
+          <button
+            class="outline-dashed rounded-sm dark:hover:bg-slate-700 hover:bg-slate-400 w-2/3"
+            type="submit"
+          >
+            Log in
+          </button>
+        </div>
+      </form>
     </div>
-    <div class="mb-2">
-      <label for="email">Email:</label>
-      <input
-        class="text-black"
-        type="email"
-        id="email"
-        v-model="userStore.email"
-        required
-      />
-    </div>
-    <div class="mb-2">
-      <label for="password">Password:</label>
-      <input
-        class="text-black"
-        type="password"
-        id="password"
-        v-model="userStore.password"
-        required
-      />
-    </div>
-    <button
-      class="outline-dashed rounded-sm dark:hover:bg-slate-700 hover:bg-slate-400"
-      type="submit"
-    >
-      Sign Up
-    </button>
-  </form>
-
-  <p v-if="userStore.message">{{ userStore.message }}</p>
-
-  <div v-if="userStore.user">
-    <p>Logged in as {{ userStore.user }}</p>
-
-    <button
-      @click="userStore.logOut"
-      class="outline-dashed rounded-sm dark:hover:bg-slate-700 hover:bg-slate-400"
-    >
-      Log out
-    </button>
   </div>
-
-  <form @submit.prevent="logIn">
-    <div class="mb-2">
-      <label for="username">Username:</label>
-      <input
-        class="text-black"
-        type="text"
-        id="username"
-        v-model="login_credentials.username"
-        required
-      />
-    </div>
-    <div class="mb-2">
-      <label for="password">Password:</label>
-      <input
-        class="text-black"
-        type="password"
-        id="password"
-        v-model="login_credentials.password"
-        required
-      />
-    </div>
-    <button
-      class="outline-dashed rounded-sm dark:hover:bg-slate-700 hover:bg-slate-400"
-      type="submit"
-    >
-      Log in
-    </button>
-  </form>
 </template>
