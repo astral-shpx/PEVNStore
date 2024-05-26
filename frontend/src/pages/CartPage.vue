5f4f5912a6f1a0deef0c67c73b42e20029683017
Kiril <kiril2315@gmail.com>
2024-05-26 19:49:35 +0300

    flex wrap only on products

--- frontend/src/pages/CartPage.vue
+++ frontend/src/pages/CartPage.vue
@@ -19,28 +19,31 @@ const clearCart = () => {
 
   <div class="flex justify-center">
     <aside class="hidden md:flex justify-center w-1/4">aside</aside>
-    <div class="flex flex-wrap justify-center md:w-3/5 w-full p-2">
-      <div
-        class="flex flex-col w-1/2 md:w-1/4"
-        v-for="cartProduct in cartStore.cartProducts"
-      >
+    <div class="flex flex-col justify-center md:w-3/5 w-full p-2">
+      <div class="flex flex-wrap">
+        <div v-show="cartStore.cartProducts?.length === 0">Cart is empty</div>
         <div
-          class="flex flex-col justify-between mb-4 mx-2 cursor-pointer hover:shadow-lg bg-gray-300 rounded dark:bg-gray-700 h-full max-h-96 p-2"
+          class="flex flex-col w-1/2 md:w-1/4"
+          v-for="cartProduct in cartStore.cartProducts"
         >
-          <Product :product="cartProduct" />
+          <div
+            class="flex flex-col justify-between mb-4 mx-2 cursor-pointer hover:shadow-lg bg-gray-300 rounded dark:bg-gray-700 h-full max-h-96 p-2"
+          >
+            <Product :product="cartProduct" />
 
-          <div class="flex justify-center">
-            <button
-              @click="cartStore.removeFromCart(cartProduct.id)"
-              class="flex justify-center outline-dashed rounded-sm dark:hover:bg-slate-500 hover:bg-slate-400 w-full"
-            >
-              remove
-            </button>
+            <div class="flex justify-center">
+              <button
+                @click="cartStore.removeFromCart(cartProduct.id)"
+                class="flex justify-center outline-dashed rounded-sm dark:hover:bg-slate-500 hover:bg-slate-400 w-full"
+              >
+                remove
+              </button>
+            </div>
           </div>
         </div>
       </div>
 
-      <div class="flex flex-col justify-center md:w-3/5 w-full p-2">
+      <div class="flex flex-col justify-center w-1/2 md:w-3/5 p-2">
         <p v-if="!userStotre.user">
           ! You should log in to see your cart and checkout
         </p>
