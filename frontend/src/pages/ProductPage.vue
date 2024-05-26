5895e88e4e38e63a30d70ac27b12b73c1e602992
Kiril <kiril2315@gmail.com>
2024-05-26 20:02:40 +0300

    fix product page layout

--- frontend/src/pages/ProductPage.vue
+++ frontend/src/pages/ProductPage.vue
@@ -36,59 +36,66 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="flex justify-center items-center mx-2">
-    <div>
-      <div class="flex justify-center">
-        <img
-          class="px-2 py-4"
-          :src="product?.product_image_url"
-          alt=""
-          srcset=""
-        />
-      </div>
-      <h1 class="flex justify-center mb-2">
-        {{ product?.product_name }}
-      </h1>
-      <div class="flex justify-center mb-4">
-        {{ product?.product_rating }} rating
-        {{ product?.customer_reviews }} reviews
-      </div>
-      <h2 class="flex justify-center mb-2">$ {{ product?.product_price }}</h2>
-      <div class="flex justify-center mb-4">
-        <button
-          class="flex w-2/3 justify-center mb-4 outline-dashed rounded-sm dark:hover:bg-slate-700 hover:bg-slate-400"
-          @click="addToCart"
-        >
-          Add to cart
-        </button>
-      </div>
-      <div class="mb-4 mx-2">
-        <h2>Description</h2>
-        <p>
-          {{ product?.product_description }}
-        </p>
-      </div>
+  <div class="flex justify-center">
+    <div class="mx-2 lg:w-1/2">
+      <div>
+        <div class="flex justify-center">
+          <img
+            class="px-2 py-4"
+            :src="product?.product_image_url"
+            alt=""
+            srcset=""
+          />
+        </div>
 
-      <div class="mb-4 mx-2">
-        <h2>Details</h2>
-        <tbody>
-          <tr>
-            <th>Manufacturer:</th>
-            <td>{{ product?.manufacturer }}</td>
-          </tr>
-          <tr>
-            <th>Category:</th>
-            <td>{{ product?.product_category }}</td>
-          </tr>
-          <tr>
-            <th>Website:</th>
-            <td>{{ product?.product_website }}</td>
-          </tr>
-          <tr>
-            <th>Release date:</th>
-            <td>{{ product?.release_date }}</td>
-          </tr>
-        </tbody>
+        <h1 class="flex justify-center mb-2">
+          {{ product?.product_name }}
+        </h1>
+
+        <div class="flex justify-center mb-4">
+          {{ product?.product_rating }} rating
+          {{ product?.customer_reviews }} reviews
+        </div>
+
+        <h2 class="flex justify-center mb-2">$ {{ product?.product_price }}</h2>
+
+        <div class="flex justify-center mb-4">
+          <button
+            class="flex w-2/3 md:w-1/4 justify-center mb-4 outline-dashed rounded-sm dark:hover:bg-slate-700 hover:bg-slate-400"
+            @click="addToCart"
+          >
+            Add to cart
+          </button>
+        </div>
+
+        <div class="mb-4 mx-2">
+          <h2>Description</h2>
+          <p>
+            {{ product?.product_description }}
+          </p>
+        </div>
+
+        <div class="mb-4 mx-2">
+          <h2>Details</h2>
+          <tbody>
+            <tr>
+              <th>Manufacturer:</th>
+              <td>{{ product?.manufacturer }}</td>
+            </tr>
+            <tr>
+              <th>Category:</th>
+              <td>{{ product?.product_category }}</td>
+            </tr>
+            <tr>
+              <th>Website:</th>
+              <td>{{ product?.product_website }}</td>
+            </tr>
+            <tr>
+              <th>Release date:</th>
+              <td>{{ product?.release_date }}</td>
+            </tr>
+          </tbody>
+        </div>
       </div>
     </div>
   </div>
